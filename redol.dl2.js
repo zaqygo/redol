16cb4770f36d6a8222fa225f8e852be5a2d3961a
zaqys repo <103855028+zaqygo@users.noreply.github.com>
2025-03-16 14:52:22 +0300

    Add files via upload

---
+++ redol.dl2.js
@@ -0,0 +1,29 @@
+console.log("[redol] started");
+
+//
+//
+//  common variables ---->
+
+var domain = window.location.hostname; //gets the host domain
+var dlvl3 = location.hostname.split('.')[0]; //getting a domain level 3
+var dpath = window.location.pathname; //getting full path
+var anchors = window.location.hash.substring(); //implementing a path to a specific url
+
+//  common variables ---->
+//
+//
+//  <--------------------------------------- standart redirect links -------------------------------------->
+
+if ((!localStorage.getItem('redirectDone')) && (dlvl3 !== maindlvl3) && (!userdomain.includes(dlvl3))) {
+    console.log("[redol-main] the redirect has not been performed yet");
+    switch(domain){
+        case domain: 
+            console.log("[redol-main] redirect to url:", maindomain);
+            window.location.replace("https://"+ maindomain + dpath + anchors);
+            localStorage.setItem('redirectDone', 'true');
+        break;}}
+else {console.log("[redol-main] no reasons for redirect");localStorage.removeItem('redirectDone');}
+
+//  <--------------------------------------- standart redirect links -------------------------------------->
+//
+//
