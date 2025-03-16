7c796efef75f9e1f687844d8483ef3fc1cc0b377
zaqys repo <103855028+zaqygo@users.noreply.github.com>
2025-03-16 22:57:39 +0300

    main js file

--- redol.js
+++ redol.js
@@ -8,7 +8,7 @@ var domain = window.location.hostname; //gets the host domain
 var dlvl2 = location.hostname.split('.')[1]+'.'+location.hostname.split('.')[2]; //getting a domain, but as a second-level domain
 var dlvl3 = location.hostname.split('.')[0]; //getting a domain level 3
 var dpath = window.location.pathname; //getting full path
-var checkurl = domain + dpath2; //checking whether the user is in the userhost path
+var checkurl = domain + dpath2; //checking whether the user is in the redirhost path
 var anchors = window.location.hash.substring(); //implementing a path to a specific url
 var hostingfp = (anchors.replace(/\#/g, '')).replace(/\$/g, '#'); //removing unnecessary characters
 
@@ -21,7 +21,7 @@ if ((!localStorage.getItem('redirectDone')) && (checkurl !== maincheckurl) && (d
     var targetUrl = "https://"+turl;
     var targetDomain = "https://"+turldomain;
     console.log("[redol] the redirect has not been performed yet");
-    if ((dlvl3 !== maindlvl3) && (userdomain.includes(dlvl3))) {
+    if ((dlvl3 !== maindlvl3) && (redirectdomain.includes(dlvl3))) {
     switch (window.location.hostname) {
         case (domain):
             if (hostingfp !== '') {targetUrl += "/" + hostingfp;}
@@ -42,7 +42,7 @@ else {console.log("[redol] the redirect has already been performed earlier");loc
 //
 //  <--------------------------------------- standart redirect links -------------------------------------->
 
-if ((!localStorage.getItem('redirectDone')) && (dlvl3 !== maindlvl3 || dlvl2 !== maindlvl2) && (!userdomain.includes(dlvl3))) {
+if ((!localStorage.getItem('redirectDone')) && (dlvl3 !== maindlvl3) && (dlvl2 !== maindlvl2) && (!redirectdomain.includes(dlvl3))) {
     console.log("[redol-main] the redirect has not been performed yet");
     switch(domain){
         case domain: 
